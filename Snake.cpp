81180966880670e4c00f4705782f411ef1bd6328
Caleb Wagner <calebwagner200@gmail.com>
2024-08-20 20:40:02 +0200

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,18 +1,20 @@
-//C++ Tutorial Simple Snake Game - NVitanovic
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <ctime>
 using namespace std;
+
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirecton dir;
-void Setup()
-{
+
+void Setup() {
+    srand(time(0));  // Seed the random number generator
     gameOver = false;
     dir = STOP;
     x = width / 2;
@@ -21,30 +23,25 @@ void Setup()
     fruitY = rand() % height;
     score = 0;
 }
-void Draw()
-{
-    system("cls"); //system("clear");
-    for (int i = 0; i < width+2; i++)
+
+void Draw() {
+    system("cls");
+    for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             if (j == 0)
                 cout << "#";
             if (i == y && j == x)
                 cout << "O";
             else if (i == fruitY && j == fruitX)
                 cout << "F";
-            else
-            {
+            else {
                 bool print = false;
-                for (int k = 0; k < nTail; k++)
-                {
-                    if (tailX[k] == j && tailY[k] == i)
-                    {
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
                         cout << "o";
                         print = true;
                     }
@@ -52,35 +49,32 @@ void Draw()
                 if (!print)
                     cout << " ";
             }
-
             if (j == width - 1)
                 cout << "#";
         }
         cout << endl;
     }
 
-    for (int i = 0; i < width+2; i++)
+    for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
     cout << "Score:" << score << endl;
 }
-void Input()
-{
-    if (_kbhit())
-    {
-        switch (_getch())
-        {
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
         case 'a':
-            dir = LEFT;
+            if (dir != RIGHT) dir = LEFT;
             break;
         case 'd':
-            dir = RIGHT;
+            if (dir != LEFT) dir = RIGHT;
             break;
         case 'w':
-            dir = UP;
+            if (dir != DOWN) dir = UP;
             break;
         case 's':
-            dir = DOWN;
+            if (dir != UP) dir = DOWN;
             break;
         case 'x':
             gameOver = true;
@@ -88,15 +82,14 @@ void Input()
         }
     }
 }
-void Logic()
-{
+
+void Logic() {
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
     tailX[0] = x;
     tailY[0] = y;
-    for (int i = 1; i < nTail; i++)
-    {
+    for (int i = 1; i < nTail; i++) {
         prev2X = tailX[i];
         prev2Y = tailY[i];
         tailX[i] = prevX;
@@ -104,8 +97,7 @@ void Logic()
         prevX = prev2X;
         prevY = prev2Y;
     }
-    switch (dir)
-    {
+    switch (dir) {
     case LEFT:
         x--;
         break;
@@ -121,8 +113,7 @@ void Logic()
     default:
         break;
     }
-    //if (x > width || x < 0 || y > height || y < 0)
-    //  gameOver = true;
+
     if (x >= width) x = 0; else if (x < 0) x = width - 1;
     if (y >= height) y = 0; else if (y < 0) y = height - 1;
 
@@ -130,23 +121,21 @@ void Logic()
         if (tailX[i] == x && tailY[i] == y)
             gameOver = true;
 
-    if (x == fruitX && y == fruitY)
-    {
+    if (x == fruitX && y == fruitY) {
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
         nTail++;
     }
 }
-int main()
-{
+
+int main() {
     Setup();
-    while (!gameOver)
-    {
+    while (!gameOver) {
         Draw();
         Input();
         Logic();
-        Sleep(10); //sleep(10);
+        Sleep(100);
     }
     return 0;
 }
